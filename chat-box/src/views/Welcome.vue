b6bf0ded16cca0cc0bd4bcd724ad0a67f6cf35bf
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-25 19:01:34 -0700

    added signup and login form components

--- chat-box/src/views/Welcome.vue
+++ chat-box/src/views/Welcome.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="welcome container">
       <p>welcome</p>
+      <SignupForm/>
+      <LoginForm/>
   </div>
 </template>
 
 <script>
-export default {
 
+import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
+export default {
+    components: { SignupForm, LoginForm}
 }
 </script>
 

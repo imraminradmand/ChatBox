81dbd2c01fff6d4f9979275d9c45c051cd022f3b
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-25 19:06:30 -0700

    some styling changes to main page

--- chat-box/src/views/Welcome.vue
+++ chat-box/src/views/Welcome.vue
@@ -18,6 +18,23 @@ export default {
 <style>
     .welcome {
         text-align: center;
-        padding: 20px 0px;
+        padding: 20px 0;
+    }
+    .welcome form {
+        width: 250px;
+        margin: 20px auto;
+    }
+    .welcome label {
+        display: block;
+        margin: 20px 0 10px;
+    }
+    .welcome input {
+        width: 100%;
+        padding: 10px;
+        border-radius: 20px;
+        border: 1px solid #eee;
+        outline: none;
+        color: #999;
+        margin: 10px auto;
     }
 </style>

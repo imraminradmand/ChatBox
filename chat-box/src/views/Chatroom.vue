341933240649e64d97d7109c30935c4ac4868ba0
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-27 21:38:10 -0700

    redirecting user to login page after logout

--- chat-box/src/views/Chatroom.vue
+++ chat-box/src/views/Chatroom.vue
@@ -6,9 +6,24 @@
 
 <script>
 
+
 import Nav from '../components/Nav.vue'
+import getCurrentUser from '../reusables/getCurrentUser'
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
+
 export default {
-    components: { Nav }
+    components: { Nav },
+
+    setup() {
+        const { user } = getCurrentUser()
+        const router = useRouter()
+        watch(user, () => {
+            if(!user.value) {
+                router.push('/')
+            }
+        })
+    }
 }
 </script>
 

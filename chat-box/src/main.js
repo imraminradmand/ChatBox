f9fab12e393b44b42e246f40738acd9a90a277cb
Ramin Radmand <ramin.radmand@gmail.com>
2020-12-27 21:23:24 -0700

    fixed refresh issue with firebase auth and route gaurd

--- chat-box/src/main.js
+++ chat-box/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+import { chatAuth } from './firebase/config'
 
-createApp(App).use(router).mount('#app')
+let app 
+
+chatAuth.onAuthStateChanged(() => {
+    if(!app) {
+        app = createApp(App).use(router).mount('#app')
+    }
+})
